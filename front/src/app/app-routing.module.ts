d3a03803858de9227f480578412b424deb837f59
muhammed-sayeed <sayeedmon25@gmail>
2023-05-30 09:27:57 +0530

    components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './featureModule/user/components/home/home.component';
+import { LoginComponent } from './featureModule/user/components/login/login.component';
+import { SignUpComponent } from './featureModule/user/components/sign-up/sign-up.component';
 
 const routes: Routes = [
-  
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignUpComponent}
 ];
 
 @NgModule({
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './featureModule/user/components/login/login/login.component';
+import { LoginComponent } from './featureModule/user/components/login/login.component';
 import { SignUpComponent } from './featureModule/user/components/sign-up/sign-up.component';
+import { HomeComponent } from './featureModule/user/components/home/home.component';
+import { LayoutComponent } from './featureModule/user/components/layout/layout.component';
+import { NavbarComponent } from './coreModule/components/user/navbar/navbar.component';
+import { SidebarComponent } from './coreModule/components/user/sidebar/sidebar.component';
+import { FooterComponent } from './coreModule/components/user/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent,
+    LayoutComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
